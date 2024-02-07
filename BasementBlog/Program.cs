26740ce7f066d51c3d48843105c6f2e98d2025e0
Viet Hoang <vhoang@zoodata.com.au>
2024-02-07 22:57:44 +0800

    Added Auth0 authentication

--- BasementBlog/Program.cs
+++ BasementBlog/Program.cs
@@ -1,13 +1,16 @@
+using Auth0.AspNetCore.Authentication;
 using Autofac;
 using Autofac.Extensions.DependencyInjection;
-using BasementBlog.Components;
 using BasementBlog.Database;
 using BasementBlog.Services.Modules;
 using BasementBlog.ViewModels;
 using Blazorise;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using MudBlazor;
 using MudBlazor.Services;
+using BasementBlog.Components;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -30,7 +33,32 @@ builder.Services
     .AddBlazorise()
     .AddMudMarkdownServices();
 
+builder.Services.AddAuth0WebAppAuthentication(o =>
+{
+    o.Domain = builder.Configuration["Auth0:Domain"];
+    o.ClientId = builder.Configuration["Auth0:ClientId"];
+});
+
 var app = builder.Build();
+
+app.MapGet("/Account/Login", async (HttpContext httpContext, string redirectUri = "/") =>
+{
+    var authenticationProperties = new LoginAuthenticationPropertiesBuilder()
+            .WithRedirectUri(redirectUri)
+            .Build();
+
+    await httpContext.ChallengeAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+});
+
+app.MapGet("/Account/Logout", async (HttpContext httpContext, string redirectUri = "/") =>
+{
+    var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+            .WithRedirectUri(redirectUri)
+            .Build();
+
+    await httpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+    await httpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+});
 
 using (var scope = ((IApplicationBuilder)app).ApplicationServices.GetService<IServiceScopeFactory>()!.CreateScope())
 {
