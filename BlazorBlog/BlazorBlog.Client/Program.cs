d61467dc37d39756b92d2c029b981d34d50581d4
Viet Hoang <vhoang@zoodata.com.au>
2024-08-10 18:52:36 +0800

    Clean up

--- BlazorBlog/BlazorBlog.Client/Program.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BlazorBlog.Client.Services;
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<IPostServices, PostServices>();
-
-await builder.Build().RunAsync();
