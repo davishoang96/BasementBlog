27ec43e3602a9a0c41a94f066b94d596df392bc7
Viet Hoang <vhoang@zoodata.com.au>
2024-08-14 21:08:32 +0800

    register

--- Blog/Blog.Client/Program.cs
+++ Blog/Blog.Client/Program.cs
@@ -3,4 +3,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.Services.AddSingleton<IPostServices, PostServices>();
+
 await builder.Build().RunAsync();
