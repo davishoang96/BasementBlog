f4089c78f58612fb831ebe16e1a84f65b18bbefa
Viet Hoang <vhoang@zoodata.com.au>
2024-08-15 11:43:41 +0800

    test

--- Blog/Blog.Client/Program.cs
+++ Blog/Blog.Client/Program.cs
@@ -3,15 +3,15 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
-//var handler = new HttpClientHandler
-//{
-//    // Bypass SSL certificate validation
-//    ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
-//};
+var handler = new HttpClientHandler
+{
+    // Bypass SSL certificate validation
+    ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true
+};
 
 builder.Services.AddHttpClient<IPostServices, PostServices>(c =>
 {
     c.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress);
-});
+}).ConfigurePrimaryHttpMessageHandler(() => handler);
 
 await builder.Build().RunAsync();
