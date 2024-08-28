e8f92cba22d05e141f6d5e6ab6bba430b867a2d0
Viet Hoang <vhoang@zoodata.com.au>
2024-08-28 19:59:19 +0800

    init

---
+++ Blog.DTO/UserInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Blog.DTO;
+
+public class UserInfo
+{
+    public string UserId { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Role { get; set; }
+}
--- Blog/Blog.Client/AuthenticationStateSyncer/PersistentAuthenticationStateProvider.cs
+++ Blog/Blog.Client/AuthenticationStateSyncer/PersistentAuthenticationStateProvider.cs
@@ -1,3 +1,4 @@
+using Blog.DTO;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using System.Security.Claims;
@@ -6,23 +7,26 @@ namespace Blog.Client.AuthenticationStateSyncer;
 
 public class PersistentAuthenticationStateProvider(PersistentComponentState persistentState) : AuthenticationStateProvider
 {
-  private static readonly Task<AuthenticationState> _unauthenticatedTask =
-      Task.FromResult(new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity())));
+    private static readonly Task<AuthenticationState> _unauthenticatedTask =
+        Task.FromResult(new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity())));
 
-  public override Task<AuthenticationState> GetAuthenticationStateAsync()
-  {
-    if (!persistentState.TryTakeFromJson<UserInfo>(nameof(UserInfo), out var userInfo) || userInfo is null)
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
     {
-      return _unauthenticatedTask;
-    }
+        if (!persistentState.TryTakeFromJson<UserInfo>(nameof(UserInfo), out var userInfo) || userInfo is null)
+        {
+            return _unauthenticatedTask;
+        }
 
-    Claim[] claims = [
-        new Claim(ClaimTypes.NameIdentifier, userInfo.UserId),
-        new Claim(ClaimTypes.Name, userInfo.Name ?? string.Empty),
-        new Claim(ClaimTypes.Email, userInfo.Email ?? string.Empty)];
+        Claim[] claims =
+        [
+            new Claim(ClaimTypes.NameIdentifier, userInfo.UserId),
+            new Claim(ClaimTypes.Name, userInfo.Name ?? string.Empty),
+            new Claim(ClaimTypes.Email, userInfo.Email ?? string.Empty),
+            new Claim(ClaimTypes.Role, userInfo.Role ?? string.Empty)
+        ];
 
-    return Task.FromResult(
+        return Task.FromResult(
         new AuthenticationState(new ClaimsPrincipal(new ClaimsIdentity(claims,
             authenticationType: nameof(PersistentAuthenticationStateProvider)))));
-  }
+    }
 }
