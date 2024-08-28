73ecb0cf4d5b9273b54ed98879dc0ffdaffb5bce
Viet Hoang <vhoang@zoodata.com.au>
2024-08-28 08:10:44 +0800

    init

---
+++ Blog/Blog/Controllers/WorkLogController.cs
@@ -0,0 +1,47 @@
+﻿using Blog.DTO;
+using Blog.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Blog.Controllers;
+
+[Route("[controller]")]
+public class WorkLogController : BaseAuthorizedController
+{
+    private IWorkLogRepository workLogRepository;
+    public WorkLogController(IWorkLogRepository workLogRepository)
+    {
+        this.workLogRepository = workLogRepository;
+    }
+
+    [HttpGet(nameof(GetWorkLogById))]
+    [SwaggerOperation(OperationId = nameof(GetWorkLogById))]
+    public async Task<ActionResult<WorkLogDTO>> GetWorkLogById(string id)
+    {
+        var result = await workLogRepository.GetWorkLogById(id);
+        return Ok(result);
+    }
+
+    [HttpGet(nameof(GetAllWorkLogs))]
+    [SwaggerOperation(OperationId = nameof(GetAllWorkLogs))]
+    public async Task<ActionResult<IEnumerable<WorkLogDTO>>> GetAllWorkLogs()
+    {
+        var result = await workLogRepository.GetAllWorkLogs();
+        return Ok(result);
+    }
+
+    [HttpPost(nameof(SaveOrUpdateWorkLog))]
+    [Produces("text/plain")]
+    [SwaggerOperation(OperationId = nameof(SaveOrUpdateWorkLog))]
+    public async Task<string> SaveOrUpdateWorkLog([FromBody]WorkLogDTO workLogDTO)
+    {
+        var result = await workLogRepository.SaveOrUpdateWorkLog(workLogDTO);   
+        return result;
+    }
+
+    [HttpDelete]
+    public async Task<bool> ClearAllWorkLogs()
+    {
+        return await workLogRepository.ClearAllWorkLogs();
+    }
+}
