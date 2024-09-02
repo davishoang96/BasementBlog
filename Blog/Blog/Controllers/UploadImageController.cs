244f7e7ba95367ba9ed469c81c4026cafd490642
Viet Hoang <vhoang@zoodata.com.au>
2024-09-02 12:41:06 +0800

    init

---
+++ Blog/Blog/Controllers/UploadImageController.cs
@@ -0,0 +1,19 @@
+﻿using Blog.DTO;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Blog.Controllers;
+
+[Route("[controller]")]
+public class UploadImageController : BaseAuthorizedController
+{
+    public UploadImageController() { }
+
+    [HttpPost(nameof(UploadImage))]
+    [Produces("text/plain")]
+    [SwaggerOperation(OperationId = nameof(UploadImage))]
+    public async Task<string> UploadImage([FromBody] ImageDTO imageDTO)
+    {
+        return imageDTO.Name;
+    }
+}
