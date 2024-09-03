25c270c59085d8267b69859f31bfee8b5386392b
Davis Hoang <davishoang96@gmail.com>
2024-09-03 08:33:17 +0800

    Add upload image api (#105)
    * init
    
    * update
    
    * image directory
    
    * ImageDirectoryPath
    
    * Use skiasharp to write imageBytes to disk
    
    * SkiaSharp.NativeAssets.Linux.NoDependencies
    
    * filePath
    
    * test
    
    * init
    
    * test
    
    ---------
    
    Co-authored-by: Viet Hoang <vhoang@zoodata.com.au>

--- Blog.Services/BlogApiClient.cs
+++ Blog.Services/BlogApiClient.cs
@@ -119,6 +119,15 @@ namespace Blog.Services
 
         /// <returns>OK</returns>
         /// <exception cref="ApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<string> UploadImageAsync(ImageDTO body);
+
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>OK</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        System.Threading.Tasks.Task<string> UploadImageAsync(ImageDTO body, System.Threading.CancellationToken cancellationToken);
+
+        /// <returns>OK</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
         System.Threading.Tasks.Task<WorkLogDTO> GetWorkLogByIdAsync(string id);
 
         /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
@@ -959,6 +968,85 @@ namespace Blog.Services
                     if (!string.IsNullOrEmpty(_baseUrl)) urlBuilder_.Append(_baseUrl);
                     // Operation Path: "Post/savePost"
                     urlBuilder_.Append("Post/savePost");
+
+                    PrepareRequest(client_, request_, urlBuilder_);
+
+                    var url_ = urlBuilder_.ToString();
+                    request_.RequestUri = new System.Uri(url_, System.UriKind.RelativeOrAbsolute);
+
+                    PrepareRequest(client_, request_, url_);
+
+                    var response_ = await client_.SendAsync(request_, System.Net.Http.HttpCompletionOption.ResponseHeadersRead, cancellationToken).ConfigureAwait(false);
+                    var disposeResponse_ = true;
+                    try
+                    {
+                        var headers_ = new System.Collections.Generic.Dictionary<string, System.Collections.Generic.IEnumerable<string>>();
+                        foreach (var item_ in response_.Headers)
+                            headers_[item_.Key] = item_.Value;
+                        if (response_.Content != null && response_.Content.Headers != null)
+                        {
+                            foreach (var item_ in response_.Content.Headers)
+                                headers_[item_.Key] = item_.Value;
+                        }
+
+                        ProcessResponse(client_, response_);
+
+                        var status_ = (int)response_.StatusCode;
+                        if (status_ == 200)
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            var result_ = (string)System.Convert.ChangeType(responseData_, typeof(string));
+                            return result_;
+                        }
+                        else
+                        {
+                            var responseData_ = response_.Content == null ? null : await response_.Content.ReadAsStringAsync().ConfigureAwait(false);
+                            throw new ApiException("The HTTP status code of the response was not expected (" + status_ + ").", status_, responseData_, headers_, null);
+                        }
+                    }
+                    finally
+                    {
+                        if (disposeResponse_)
+                            response_.Dispose();
+                    }
+                }
+            }
+            finally
+            {
+                if (disposeClient_)
+                    client_.Dispose();
+            }
+        }
+
+        /// <returns>OK</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        public virtual System.Threading.Tasks.Task<string> UploadImageAsync(ImageDTO body)
+        {
+            return UploadImageAsync(body, System.Threading.CancellationToken.None);
+        }
+
+        /// <param name="cancellationToken">A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        /// <returns>OK</returns>
+        /// <exception cref="ApiException">A server side error occurred.</exception>
+        public virtual async System.Threading.Tasks.Task<string> UploadImageAsync(ImageDTO body, System.Threading.CancellationToken cancellationToken)
+        {
+            var client_ = _httpClient;
+            var disposeClient_ = false;
+            try
+            {
+                using (var request_ = new System.Net.Http.HttpRequestMessage())
+                {
+                    var json_ = Newtonsoft.Json.JsonConvert.SerializeObject(body, JsonSerializerSettings);
+                    var content_ = new System.Net.Http.StringContent(json_);
+                    content_.Headers.ContentType = System.Net.Http.Headers.MediaTypeHeaderValue.Parse("application/json");
+                    request_.Content = content_;
+                    request_.Method = new System.Net.Http.HttpMethod("POST");
+                    request_.Headers.Accept.Add(System.Net.Http.Headers.MediaTypeWithQualityHeaderValue.Parse("text/plain"));
+
+                    var urlBuilder_ = new System.Text.StringBuilder();
+                    if (!string.IsNullOrEmpty(_baseUrl)) urlBuilder_.Append(_baseUrl);
+                    // Operation Path: "UploadImage/UploadImage"
+                    urlBuilder_.Append("UploadImage/UploadImage");
 
                     PrepareRequest(client_, request_, urlBuilder_);
 
---
+++ Blog/Blog/Controllers/UploadImageController.cs
@@ -0,0 +1,46 @@
+﻿using Blog.DTO;
+using Microsoft.AspNetCore.Mvc;
+using SkiaSharp;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Drawing;
+
+namespace Blog.Controllers;
+
+[Route("[controller]")]
+public class UploadImageController : BaseAuthorizedController
+{
+    private readonly IConfiguration configuration;
+    public UploadImageController(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+    }
+
+    [HttpPost(nameof(UploadImage))]
+    [Produces("text/plain")]
+    [SwaggerOperation(OperationId = nameof(UploadImage))]
+    public async Task<string> UploadImage([FromBody] ImageDTO imageDTO)
+    {
+        var dict = configuration["ImageDirectoryPath"];
+        if (!Path.Exists(dict))
+        {
+            Directory.CreateDirectory(dict);
+        }
+
+        Guid guid = Guid.NewGuid();
+        imageDTO.Name = guid.ToString();
+        var filePath = Path.Combine(dict, $"{imageDTO.Name}.jpg");
+        byte[] imageBytes = Convert.FromBase64String(imageDTO.Data);
+
+        // Use skiasharp to write imageBytes to disk
+        using (var stream = new SKMemoryStream(imageBytes))
+        using (var bitmap = SKBitmap.Decode(stream))
+        using (var image = SKImage.FromBitmap(bitmap))
+        using (var data = image.Encode(SKEncodedImageFormat.Jpeg, 100)) // 100 is the quality
+        using (var fileStream = System.IO.File.OpenWrite(filePath))
+        {
+            data.SaveTo(fileStream);
+        }
+
+        return $"![Image-{DateTime.Now.ToShortDateString()}](/Images/{imageDTO.Name}.jpg){{ width={100}% }}"; ;
+    }
+}
