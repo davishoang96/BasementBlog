451b938ba0319c7886d30e871fc43e43194255ca
Davis Hoang <davishoang96@gmail.com>
2024-11-20 22:56:32 +0800

    Update mudblazor to 8.0.0-preview (#122)

--- Blog.Services/Modules/ServiceModules.cs
+++ Blog.Services/Modules/ServiceModules.cs
@@ -9,7 +9,6 @@ public class ServiceModules : Module
         base.Load(builder);
 
         builder.RegisterType<MarkdownService>().AsImplementedInterfaces().InstancePerDependency();
-        builder.RegisterType<BlogDialogService>().AsImplementedInterfaces().InstancePerDependency();
         builder.RegisterType<SqidService>().AsImplementedInterfaces().InstancePerDependency();
         builder.RegisterType<FileService>().AsImplementedInterfaces().InstancePerDependency();
     }
